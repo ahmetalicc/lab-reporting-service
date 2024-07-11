9b6f1357352ba480997d36926a510a89d0d7ef68
Ahmet Alıç <ahmet.alic@ogr.sakarya.edu.tr>
2024-07-12 01:22:53 +0300

    Chore: Initial commit.

---
+++ src/main/java/org/backend/project/reportingapp/ReportingAppApplication.java
@@ -0,0 +1,13 @@
+package org.backend.project.reportingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingAppApplication.class, args);
+    }
+
+}
